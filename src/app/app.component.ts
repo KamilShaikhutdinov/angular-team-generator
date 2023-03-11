c5625e5a715fa15c958cf616c7da97af3ba8a5dd
Kamil Shaykhutdinov <kamil@spotnana.com>
2023-03-11 20:52:47 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'teamsapp';
+  newMemberName = '';
+  members: string[] = [];
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+}
