10f37d760f69d2db20e575e2aa9c23812c917fd1
KamilShaikhutdinov <kam1l@yandex.ru>
2023-03-12 17:06:01 +0300

    teams logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   newMemberName = '';
   members: string[] = [];
   errorMessage = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   addMember() {
     if (!this.newMemberName) {
@@ -23,5 +25,39 @@ export class AppComponent {
 
   onInput(member: string) {
     this.newMemberName = member;
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = 'Invalid number of teams';
+      return;
+    }
+    if (this.numberOfTeams > this.members.length) {
+      this.errorMessage = 'Not enough members';
+      return;
+    }
+    this.errorMessage = '';
+    const allMembers = [...this.members];
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+    this.members = [];
+    this.numberOfTeams = '';
   }
 }
