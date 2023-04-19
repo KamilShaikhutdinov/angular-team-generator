c8db4fb9c8fc41cff92a00b63766013d061890d9
Kamil Shaykhutdinov <kamil.shaykhutdinov@akvelon.com>
2023-04-19 16:55:01 +0200

    date fix

--- scripts/display-stats.js
+++ scripts/display-stats.js
@@ -5,20 +5,22 @@ const { getOctokit } = require("@actions/github");
 async function run() {
   try {
     const octokit = getOctokit(process.env.GITHUB_TOKEN);
-    const since = new Date("2023-04-04");
-    const sinceISO = since.toISOString();
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    const since = oneMonthAgo.toISOString();
+
     const { data: issues } = await octokit.rest.issues.listForRepo({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       state: "all",
-      since: core.getInput(sinceISO),
+      since,
     });
 
     const { data: pullRequests } = await octokit.rest.pulls.list({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       state: "all",
-      since: core.getInput(sinceISO),
+      since,
     });
 
     const totalIssues = issues.length;
@@ -34,11 +36,18 @@ async function run() {
     ).length;
     const closedPRs = pullRequests.filter((pr) => pr.state === "closed").length;
 
+    const issueAge = core.getInput("issue-age");
+    const filteredIssues = issues.filter((issue) => {
+      const createdDate = new Date(issue.created_at);
+      const diffInDays = (new Date() - createdDate) / (1000 * 60 * 60 * 24);
+      return diffInDays <= issueAge;
+    });
+
     console.log(`Total PRs/Issues: ${totalPRs + totalIssues}`);
     console.log(`Open PRs/Issues: ${openedPRs + openedIssues}`);
     console.log(`Closed PRs/Issues: ${closedPRs + closedIssues}`);
     console.log(
-      `PRs/Issues opened in the last month: ${openedPRs + openedIssues}`
+      `PRs/Issues opened in the last ${issueAge} days: ${filteredIssues.length}`
     );
   } catch (error) {
     core.setFailed(error.message);
