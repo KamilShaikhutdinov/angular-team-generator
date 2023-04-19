cb89af5eea1818e0051f6dee7b03d748b214a05f
Kamil Shaykhutdinov <kamil.shaykhutdinov@akvelon.com>
2023-04-19 16:26:15 +0200

    fix

--- scripts/display-stats.js
+++ scripts/display-stats.js
@@ -1,41 +1,36 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-async function run(): Promise<void> {
+async function run() {
   try {
-    const token = core.getInput('github-token');
+    const token = core.getInput("github-token");
     const octokit = github.getOctokit(token);
-
     const { data: issues } = await octokit.rest.issues.listForRepo({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      state: 'all',
-      since: core.getInput('issue-age'),
+      state: "all",
+      since: core.getInput("issue-age"),
     });
 
     const { data: pullRequests } = await octokit.rest.pulls.list({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      state: 'all',
-      since: core.getInput('issue-age'),
+      state: "all",
+      since: core.getInput("issue-age"),
     });
 
     const totalIssues = issues.length;
     const totalPRs = pullRequests.length;
 
     const openedIssues = issues.filter(
-      (issue: { state: string }) => issue.state === 'open'
+      (issue) => issue.state === "open"
     ).length;
-    const openedPRs = pullRequests.filter(
-      (pr: { state: string }) => pr.state === 'open'
-    ).length;
+    const openedPRs = pullRequests.filter((pr) => pr.state === "open").length;
 
     const closedIssues = issues.filter(
-      (issue: { state: string }) => issue.state === 'closed'
+      (issue) => issue.state === "closed"
     ).length;
-    const closedPRs = pullRequests.filter(
-      (pr: { state: string }) => pr.state === 'closed'
-    ).length;
+    const closedPRs = pullRequests.filter((pr) => pr.state === "closed").length;
 
     console.log(`Total PRs/Issues: ${totalPRs + totalIssues}`);
     console.log(`Open PRs/Issues: ${openedPRs + openedIssues}`);
@@ -43,7 +38,7 @@ async function run(): Promise<void> {
     console.log(
       `PRs/Issues opened in the last month: ${openedPRs + openedIssues}`
     );
-  } catch (error: any) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
