34a77b2c58c52241cdc98c226e512b7076d263da
Kamil Shaykhutdinov <kamil.shaykhutdinov@akvelon.com>
2023-04-19 15:44:45 +0200

    pr stats

---
+++ scripts/display-stats.ts
@@ -0,0 +1,54 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run(): Promise<void> {
+  try {
+    const octokit = github.getOctokit(core.getInput('github-token'));
+    const since = new Date(
+      Date.now() - parseInt(core.getInput('issue-age')) * 24 * 60 * 60 * 1000
+    ).toISOString();
+
+    const { data: issues } = await octokit.rest.issues.listForRepo({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      state: 'all',
+      since,
+      per_page: 100,
+    });
+
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      state: 'all',
+      since,
+      per_page: 100,
+    });
+
+    const totalIssues = issues.length;
+    const openedIssues = issues.filter(
+      (issue: { state: string }) => issue.state === 'open'
+    ).length;
+    const closedIssues = issues.filter(
+      (issue: { state: string }) => issue.state === 'closed'
+    ).length;
+
+    const totalPRs = pullRequests.length;
+    const openedPRs = pullRequests.filter(
+      (pr: { state: string }) => pr.state === 'open'
+    ).length;
+    const closedPRs = pullRequests.filter(
+      (pr: { state: string }) => pr.state === 'closed'
+    ).length;
+
+    console.log(`Total PRs & Issues: ${totalPRs + totalIssues}`);
+    console.log(`Open PRs & Issues: ${openedPRs + openedIssues}`);
+    console.log(`Closed PRs & Issues: ${closedPRs + closedIssues}`);
+    console.log(
+      `PRs & Issues opened in the last month: ${openedPRs + openedIssues}`
+    );
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
